c6a2a7bf8a39df9143c971a3b9d5bbed477eb163
Chikara <chtakahashi10@gmail.com>
2021-04-19 14:27:31 -0400

    enables executable permissions on scriptRunner.sh

--- scriptRunner.sh
+++ scriptRunner.sh
@@ -1,3 +1,5 @@
 #! /usr/bin/env bash
 
-echo "testing Inspec scan"
+set -o pipefail
+
+inspec exec /home/default/profiles/cms-ars-3.1-moderate-aws-rds-oracle-mysql-ee-5.7-cis-overlay --target aws:// --chef-license accept-silent --no-color
