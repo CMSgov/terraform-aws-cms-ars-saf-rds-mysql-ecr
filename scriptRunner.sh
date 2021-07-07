63ac181239cf5fc7769e2a07b7283cf28fa1dad9
Rupinder Dhariwal <rd@truss.works>
2021-07-07 11:24:05 -0400

    conditionalize security hub collector integeration using Product Arn variable

--- scriptRunner.sh
+++ scriptRunner.sh
@@ -17,10 +17,17 @@ cinc-auditor exec /home/default/profiles/cms-ars-3.1-moderate-aws-rds-oracle-mys
 if [[ -n $s3_bucket_path ]]; then
     echo "s3_bucket_path values found: $s3_bucket_path"
     filename="$(date '+%Y-%m-%d-%H-%M-%S').json"
-    cinc-auditor exec /home/default/profiles/cms-ars-3.1-moderate-aws-rds-oracle-mysql-ee-5.7-cis-overlay --input-file=/home/default/profiles/cms-ars-3.1-moderate-aws-rds-oracle-mysql-ee-5.7-cis-overlay/inputs.yml --reporter json --chef-license accept-silent | /home/default/profiles/cms-ars-3.1-moderate-aws-rds-oracle-mysql-ee-5.7-cis-overlay/main -dry=0 -accountid=$AWSACCOUNTID -product-arn=$PRODUCTARN -rds-arn=$RDSARN
+    cinc-auditor exec /home/default/profiles/cms-ars-3.1-moderate-aws-rds-oracle-mysql-ee-5.7-cis-overlay --input-file=/home/default/profiles/cms-ars-3.1-moderate-aws-rds-oracle-mysql-ee-5.7-cis-overlay/inputs.yml --reporter json --chef-license accept-silent | aws s3 cp - "$s3_bucket_path/$filename"
     echo "s3 scan results upload complete"
 else
     echo "s3_bucket_path variable not found, skipping s3 results upload."
+fi
+
+echo "uploading findings into security hub"
+
+if [[ -n $PRODUCTARN ]]; then
+  echo "$PRODUCTARN values found: $PRODUCTARN. Uploading findings into security hub"
+  cinc-auditor exec /home/default/profiles/cms-ars-3.1-moderate-aws-rds-oracle-mysql-ee-5.7-cis-overlay --input-file=/home/default/profiles/cms-ars-3.1-moderate-aws-rds-oracle-mysql-ee-5.7-cis-overlay/inputs.yml --reporter json --chef-license accept-silent | /home/default/profiles/cms-ars-3.1-moderate-aws-rds-oracle-mysql-ee-5.7-cis-overlay/main -dry=0 -accountid=$AWSACCOUNTID -product-arn=$PRODUCTARN -rds-arn=$RDSARN
 fi
 
 echo "cinc-auditor scan completed successfully"
