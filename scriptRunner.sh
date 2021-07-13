fae8012c8a6cf92f3d7ee058de59cd3160a639dd
M. Adam Kendall <adam@truss.works>
2021-07-13 15:22:11 -0400

    Let's make sure we only run cinc-auditor the one time

--- scriptRunner.sh
+++ scriptRunner.sh
@@ -3,21 +3,29 @@
 # similar to terraform-aws-cms-ars-saf-ecr repository, removing -e flag to account for nonzero error codes
 # Error codes for reference: https://docs.chef.io/inspec/cli/#exec
 set -o pipefail
+OVERLAY_PATH="/home/default/profiles/cms-ars-3.1-moderate-aws-rds-oracle-mysql-ee-5.7-cis-overlay"
+INPUT_FILE="${OVERLAY_PATH}/inputs.yml"
+JSON_OUTFILE="${OVERLAY_PATH}/audit-results.json"
 
 echo "starting cinc-auditor scan"
 
 echo "hydrate config file"
 
-erb /home/default/profiles/cms-ars-3.1-moderate-aws-rds-oracle-mysql-ee-5.7-cis-overlay/inputs.yml.erb > /home/default/profiles/cms-ars-3.1-moderate-aws-rds-oracle-mysql-ee-5.7-cis-overlay/inputs.yml
+erb "${OVERLAY_PATH}/inputs.yml.erb" > "${INPUT_FILE}"
 
 # log to cloudwatch
-cinc-auditor exec /home/default/profiles/cms-ars-3.1-moderate-aws-rds-oracle-mysql-ee-5.7-cis-overlay --input-file=/home/default/profiles/cms-ars-3.1-moderate-aws-rds-oracle-mysql-ee-5.7-cis-overlay/inputs.yml --chef-license accept-silent --no-color
+
+cinc-auditor exec "${OVERLAY_PATH}" \
+  --input-file="${INPUT_FILE}" \
+  --chef-license=accept-silent \
+  --no-color \
+  --reporter=cli json:"${JSON_OUTFILE}"
 
 # shellcheck disable=SC2154
 if [[ -n $s3_bucket_path ]]; then
     echo "s3_bucket_path values found: $s3_bucket_path"
     filename="$(date '+%Y-%m-%d-%H-%M-%S').json"
-    cinc-auditor exec /home/default/profiles/cms-ars-3.1-moderate-aws-rds-oracle-mysql-ee-5.7-cis-overlay --input-file=/home/default/profiles/cms-ars-3.1-moderate-aws-rds-oracle-mysql-ee-5.7-cis-overlay/inputs.yml --reporter json --chef-license accept-silent | aws s3 cp - "$s3_bucket_path/$filename"
+    aws s3 cp "${JSON_OUTFILE}" "$s3_bucket_path/$filename"
     echo "s3 scan results upload complete"
 else
     echo "s3_bucket_path variable not found, skipping s3 results upload."
@@ -26,8 +34,13 @@ fi
 echo "uploading findings into security hub"
 
 if [[ -n $PRODUCTARN ]]; then
-  echo "$PRODUCTARN values found: $PRODUCTARN. Uploading findings into security hub"
-  cinc-auditor exec /home/default/profiles/cms-ars-3.1-moderate-aws-rds-oracle-mysql-ee-5.7-cis-overlay --input-file=/home/default/profiles/cms-ars-3.1-moderate-aws-rds-oracle-mysql-ee-5.7-cis-overlay/inputs.yml --reporter json --chef-license accept-silent | /home/default/profiles/cms-ars-3.1-moderate-aws-rds-oracle-mysql-ee-5.7-cis-overlay/main -dry=0 -accountid="$AWSACCOUNTID" -product-arn="$PRODUCTARN" -rds-arn="$RDSARN"
+  echo "PRODUCTARN values found: $PRODUCTARN. Uploading findings into security hub"
+  "${OVERLAY_PATH}/main" \
+    -dry=0 \
+    -accountid="$AWSACCOUNTID" \
+    -product-arn="$PRODUCTARN" \
+    -rds-arn="$RDSARN" \
+    < "${JSON_OUTFILE}"
 fi
 
 echo "cinc-auditor scan completed successfully"
